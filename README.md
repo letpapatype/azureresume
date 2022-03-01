1dfb827ceab6b59b12b4429327863e6d6fee6712
jovan newland <jovannewland@jovans-MacBook-Pro.local>
2022-03-01 12:41:55 -0800

    with updated HTML and added js file

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # azureresume
+First things first was updating the aesthetics,
+Not a huge ui guy, so i left the format and added my
+details. Also changed up some symbols, added a email me.
+
+The wrong the main.js, the script for the event counter
---
+++ frontend/main.js
@@ -0,0 +1,23 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    getVisitCount();
+})
+// the event of the page being loaded is the trigger to run getVisitCount 
+
+const functionApi = '';
+// this script is unsed to pull data from the CosmosDB API and return the number on the website.
+
+const getVisitCount = () => {
+    let count = 30;
+    fetch(functionApi).then(response => {
+        return response.json()
+    }).then(response => {
+        console.log("Website called function API.");
+        count = response.count;
+        document.getElementById("counter").innerText = count;
+    }).catch(function (error) {
+        console.long(error);
+    })
+    return count;
+}
+
+
